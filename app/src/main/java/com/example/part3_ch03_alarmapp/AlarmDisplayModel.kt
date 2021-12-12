7647ef7f9e636feeb39cbb0f99c42452d966e642
Yangsuhyuck <pplloo748@gmail.com>
2021-12-12 23:18:09 +0900

    2021 - 12 - 12

---
+++ app/src/main/java/com/example/part3_ch03_alarmapp/AlarmDisplayModel.kt
@@ -0,0 +1,30 @@
+package com.example.part3_ch03_alarmapp
+
+
+// data class는 생성자 부분에 인자들을 넣는다.
+
+data class AlarmDisplayModel(
+    val hour: Int,
+    val minute: Int,
+    var onOff: Boolean
+) {
+    val timeText:String
+        // getter함수
+        get() {
+        val h = "%02d".format(if ( hour < 12 ) hour else hour - 12) // 12
+        val m = "%02d".format(minute)
+
+        return "$h:$m"
+        }
+
+    val ampmText:String
+        get() {
+            return if ( hour < 12 ) "AM" else "PM"
+        }
+
+    // DB에 저장하기 위해 데이터를 원하는 형태로 가공하는 함수
+    fun makeDataForDB(): String {
+        return "$hour:$minute"
+    }
+
+}
--- app/src/main/java/com/example/part3_ch03_alarmapp/MainActivity.kt
+++ app/src/main/java/com/example/part3_ch03_alarmapp/MainActivity.kt
@@ -1,19 +1,97 @@
 package com.example.part3_ch03_alarmapp
 
+import android.app.TimePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.part3_ch03_alarmapp.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+        // step0 뷰를 초기화해주기
+        initOnOffBtn()
+        initChangeAlarmTimeBtn()
+
+        // step1 데이터 가져오기
+        // step2 뷰에 데이터를 그려주기
+
     }
+
+    // 알람 켜고 끄는 버튼 클릭
+    private fun initOnOffBtn() {
+
+        mainBinding.onOffBtn.setOnClickListener {
+            // 데이터를 확인한다.
+
+            // on / off 에 따라 작업을 처리한다.
+
+            // 오프 -> 알람 제거 , 온 -> 알람 등록
+
+            // 데이터를 저장
+        }
+
+    }
+
+    // 알람 시간 변경 버튼 클릭
+    private fun initChangeAlarmTimeBtn() {
+
+        mainBinding.changeAlarmTimeBtn.setOnClickListener {
+            // 현재 시간을 가져온다 - TimePickerDialog
+            val calendar = Calendar.getInstance()   // 현재 시간을 가져온다.
+
+            // 데이터를 저장한다.
+            // 뷰를 업데이트 한다.
+            // 기존 알람을 삭제 -> 모두 타임피커에서 시간을 선택하여 설정
+            TimePickerDialog(this, {picker , hour , minute ->
+
+                val model = saveAlarmModel(hour,minute,false)
+
+
+            }, calendar.get(Calendar.HOUR_OF_DAY) , calendar.get(Calendar.MINUTE), false).show()
+
+        }
+
+    }
+
+    // 입력한 알람 시간을 sharedPreferences를 이용하여 저장해주는 함수
+    private fun saveAlarmModel(
+        hour: Int,
+        minute: Int,
+        onOff: Boolean
+    ): AlarmDisplayModel {
+        val model = AlarmDisplayModel(
+            hour = hour,
+            minute = minute,
+            onOff = false)
+
+        val sharedPreferences = getSharedPreferences("time", Context.MODE_PRIVATE)
+        // with 범위 함수?
+        with(sharedPreferences.edit()) {
+            putString("alarm", model.makeDataForDB())
+            putBoolean("onOff", model.onOff)
+            commit()
+        }
+
+        return model
+
+    }
+
+
 }
 
 /*
 AlarmManager 사용
 Notification 사용
-Broadcast receiver 사용
+Boadcast receiver 사용
 
 Android에서 Background 작업
 백그라운드에서 알람 시간에 맞춰 진행시켜 주도록 한다
